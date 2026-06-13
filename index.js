1692d3dda919cb28ee59449a15a862a8c5fa8625
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-13 15:04:51 -0400

    new logic

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const central = require("./central");
-const vault = require("./vault");
-const dbs = require("./dbs");
+const { central, vault, dbs } = require("./database");
+
 
 // importing databases from single database file 
 // combing data across multiple databases using bracket notation
@@ -8,11 +7,11 @@ const dbs = require("./dbs");
 function retrieve(id) {
     return central(id).then((dbName) => {
         // Accessing sub database dynamically
-        return Promise.all([ dbs[dbName](id), valut(id)]);
+        return Promise.all([ dbs[dbName](id), vault(id)]);
     })
-    .then(([dbData, valutData]) => {
+    .then(([dbData, vaultData]) => {
         return {
-            id: Number(id), name: vaultData.name, username: dbData.username, email: vaultData.email, address: valutData.address, 
+            id: Number(id), name: vaultData.name, username: dbData.username, email: vaultData.email, address: vaultData.address, 
             phone: vaultData.phone, website: dbData.website, company: dbData.company 
         };
     })
@@ -25,3 +24,24 @@ function retrieve(id) {
 
 // running  Testing
 console.log("Running 2file separate structure");
+
+// valid userquery execution
+retrieve(1)
+.then((data) => {
+    console.log("\n[sucess] Retrieved data for user ID 1:");
+    console.log(JSON.stringify(data, null, 2));
+})
+.catch((error) => { 
+    console.error("\n[ERROR] Unexpected test failure:", err);
+});
+// data base failure test
+retrieve(99)
+.then((data) => {
+    console.log("\n[FAILED] Eroor test did not fire", data);
+})
+.catch((error) => {
+    console.log("\n[SUCCESS] Caught expected out of range database error:");
+    console.log("Message ->", error);
+});
+
+module.exports = retrieve;
